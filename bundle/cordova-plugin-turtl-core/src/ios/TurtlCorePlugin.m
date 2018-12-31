057cfa0d4e35a36116ee88491b445f60e7b43379
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-12-30 16:56:59 -0800

    starting to stub out ios core plugin

---
+++ bundle/cordova-plugin-turtl-core/src/ios/TurtlCorePlugin.m
@@ -0,0 +1,27 @@
+#import "turtlCorePlugin.h"
+#include "turtl_core.h"
+
+@implementation TurtlCorePlugin
+
+- (void) turtlcc_start:(CDVInvokedUrlCommand*)command {
+
+}
+
+- (void) turtlcc_send:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) turtlcc_recv:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) turtlcc_recv_event:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) turtlcc_free:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) turtlcc_lasterr:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) turtlcc_free_err:(CDVInvokedUrlCommand*)command {
+}
+
