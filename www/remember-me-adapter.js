581e8c610b277e540d951345772770b6e0a9dafa
Andrew Danger Lyon <orthecreedence@gmail.com>
2019-01-01 14:25:12 -0800

    stubbing out remember me adapter for ios

--- www/remember-me-adapter.js
+++ www/remember-me-adapter.js
@@ -1,24 +1,17 @@
-RememberMe.adapters.android_keystore = Composer.Event.extend({
+RememberMe.adapters.ios_keystore = Composer.Event.extend({
 	initialize: function(_options) {
 	},
 
 	get_login: function() {
-		return TurtlStore.load()
-			.then(function(res) {
-				var token = JSON.parse(res).key;
-				if(!token) return null;
-				var decoded = JSON.parse(atob(token));
-				return {user_id: decoded.user_id, key: decoded.key};
-			});
+		return Promise.resolve(false);
 	},
 
 	save: function(user_id, key) {
-		var token = JSON.stringify({user_id: user_id, key: key});
-		return TurtlStore.save(btoa(token));
+		return Promise.resolve(false);
 	},
 
 	clear: function() {
-		return TurtlStore.clear();
+		return Promise.resolve(false);
 	},
 });
 
