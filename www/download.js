579c474b1d0a8adea2f8aba02369bbec3ba0f0da
Andrew Danger Lyon <orthecreedence@gmail.com>
2019-01-08 00:24:51 -0800

    trying to fix file downloads in ios

--- www/download.js
+++ www/download.js
@@ -6,13 +6,6 @@ var download_blob = function(blob, options)
 {
 	options || (options = {});
 
-	// -------------------------------------------------------------------------
-	// NOTE: this not only doesn't really work at all, it screws up the indexeddb
-	// database in the app. holding out for a better way to handle files.
-	//barfr.barf('Viewing files on mobile is currently broken. We\'re working on a fix. Sorry for the trouble!');
-	//return;
-	// -------------------------------------------------------------------------
-
 	var url;
 	var name = options.name || 'download';
 	return new Promise(function(resolve, reject) {
@@ -23,7 +16,7 @@ var download_blob = function(blob, options)
 				reject(err);
 			};
 		};
-		window.resolveLocalFileSystemURL('file:///storage/emulated/0/', function(filesystem) {
+		window.resolveLocalFileSystemURL(cordova.file.documentsDirectory, function(filesystem) {
 			filesystem.getDirectory('Download', {create: true, exclusive: false}, function(dir) {
 				dir.getFile(name, {create: true, exclusive: false}, function(entry) {
 					var file_url = entry.toURL();
